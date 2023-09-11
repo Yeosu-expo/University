3831635ae30c3fc53a17492f4b8681543f35dfbc
Yeosu-expo <akqjq9250@gmail.com>
2023-09-11 15:45:14 +0900

    write java0911.md

--- 2학년 2학기/JavaPrograming/TestProject/src/testPack/App.java
+++ 2학년 2학기/JavaPrograming/TestProject/src/testPack/App.java
@@ -1,6 +1,28 @@
 package testPack;
+
+import java.util.Scanner;
+
 public class App {
+    public static double Celcious(double num){
+        double result = (num*9/5) + 32; // 섭씨에서 화씨 변환하는 수학공식
+        return result;
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final double MILE = 1.60934; // 1마일은 1.60934킬로미터 입니다.
+        
+        Scanner sc = new Scanner(System.in); // 입력 인스턴스 생성
+        for(int i=0;i<5;i++){
+            System.out.print("mile? ");
+            double mile = sc.nextDouble();
+            System.out.println("95마일은 "+MILE*mile+"킬로미터 입니다.");
+        }
+
+        System.out.println();
+
+        for(int i=0; i<5;i++){
+            System.out.print("Celcious? ");
+            double cel = sc.nextDouble();
+            System.out.println("섭씨 "+ cel +"도는 화씨 "+ Celcious(cel) + "입니다.");
+        }
     }
 }
