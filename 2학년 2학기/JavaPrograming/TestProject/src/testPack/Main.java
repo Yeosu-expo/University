274a0af83d0134488272b23ddc81410b9bbbd578
Yeosu-expo <akqjq9250@gmail.com>
2023-10-05 10:41:23 +0900

    write ds(2)1005.md

--- 2학년 2학기/JavaPrograming/TestProject/src/testPack/Main.java
+++ 2학년 2학기/JavaPrograming/TestProject/src/testPack/Main.java
@@ -1,47 +1,6 @@
 package testPack;
 
-import java.util.Scanner;
-
 public class Main{
-    
-    static void cardRun(){
-        var sc = new Scanner(System.in);
-            System.out.print("How many player? ");
-            int number = sc.nextInt();
-
-            Card players[] = new Card[number];
-
-            int[][] check = new int[4][13];
-            int shape, num;
-            for(int i=0;i<number;i++){
-                while(true){
-                    shape = (int)(Math.random()*4);
-                    num = (int)(Math.random()*12)+1;
-                    if(check[shape][num-1]==0){
-                        check[shape][num-1]=1;
-                        break;
-                    }
-                }
-                players[i] = new Card(null, 0);
-                players[i].shape = players[i].strShape(shape);
-                players[i].num = num;
-            }
-
-            for(int i=0;i<number;i++){
-                System.out.println("Player"+(i+1)+": "+players[i].shape+" "+players[i].num);
-            }
-
-            int st=0;
-            Card card = new Card(null, 0);
-            for(int i=1;i<number;i++){
-                int res = card.whoisBig(players[st], players[i]);
-                if(res == 1)
-                    st = i;
-            }
-            System.out.println("Player"+(st+1)+" won!!");
-
-            sc.close();
-    }
     public static void main(String[] args){
         Card cardArr[] = new Card[10];
 
@@ -57,15 +16,15 @@ public class Main{
         Card sortedArr[];
         sortedArr = cardArr[0].sorting(cardArr);
 
+        System.out.println("정렬 전");
+        for(int i=0;i<10;i++) {
+            cardArr[i].printCard(i+1);
+        }
 
+        System.out.println("\n정렬 후");
         for(int i=0;i<10;i++) {
             sortedArr[i].printCard(i+1);
         }
 
-        System.out.println("\n");
-
-        for(int i=0;i<10;i++) {
-            cardArr[i].printCard(i+1);
-        }
     }
 }
